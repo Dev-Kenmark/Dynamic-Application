672d9a5ab59dcb36b7999a2d899260bb11c06e22
KennyTzy <91226502+KennyTzy@users.noreply.github.com>
2022-02-21 14:56:00 +0800

    1

--- DynamicApplication/DynamicApplication/MainWindow.xaml.cs
+++ DynamicApplication/DynamicApplication/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DynamicApplication
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        
+        }
     }
 }
