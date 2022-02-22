a484a5758bd02eae74b891866c9be6650dc70bcf
eilrahc51 <98871292+eilrahc51@users.noreply.github.com>
2022-02-22 13:12:50 +0800

    constring files

--- DynamicApplication/DynamicApplication/MainWindow.xaml.cs
+++ DynamicApplication/DynamicApplication/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using Npgsql;
+using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace DynamicApplication
 {
@@ -25,8 +29,108 @@ namespace DynamicApplication
             InitializeComponent();
         }
 
+        //mssql
+        private static string mssqlfile = AppDomain.CurrentDomain.BaseDirectory + "\\mssqlFile";
+        public static string mssqlConString;
+        public static SqlConnection mscon = new SqlConnection(mssqlConString);
+        //mysql
+        private static string mysqlfile = AppDomain.CurrentDomain.BaseDirectory + "\\mysqlFile";
+        public static string mysqlConString;
+        public static MySqlConnection mycon = new MySqlConnection(mysqlConString);
+
+        //postgre
+        private static string postgrefile = AppDomain.CurrentDomain.BaseDirectory + "\\postgreFile";
+        public static string postgreConString;
+        public static NpgsqlConnection pgcon = new NpgsqlConnection(postgreConString);
+
+
+        //mssql
+        public static void MsSqlInitializeFile()
+        {
+            if (!File.Exists(mssqlfile))
+            {
+                StreamWriter sw = new StreamWriter(mssqlfile);
+                sw.WriteLine("");
+                sw.Dispose();
+                sw.Close();
+            }
+        }
+
+        public static string MsSqlRead()
+        {
+            if (!File.Exists(mssqlfile))
+            {
+                return "";
+            }
+            StreamReader sr = new StreamReader(mssqlfile);
+            string str = sr.ReadToEnd();
+            sr.Dispose();
+            sr.Close();
+
+            return str.Trim();
+        }
+
+        //mysql
+        public static void MySqlInitializeFile()
+        {
+            if (!File.Exists(mysqlfile))
+            {
+                StreamWriter sw = new StreamWriter(mysqlfile);
+                sw.WriteLine("");
+                sw.Dispose();
+                sw.Close();
+            }
+        }
+
+        public static string MySqlRead()
+        {
+            if (!File.Exists(mysqlfile))
+            {
+                return "";
+            }
+            StreamReader sr = new StreamReader(mysqlfile);
+            string str = sr.ReadToEnd();
+            sr.Dispose();
+            sr.Close();
+
+            return str.Trim();
+        }
+
+        //postgre
+        public static void PostgreInitializeFile()
+        {
+            if (!File.Exists(postgrefile))
+            {
+                StreamWriter sw = new StreamWriter(postgrefile);
+                sw.WriteLine("");
+                sw.Dispose();
+                sw.Close();
+            }
+        }
+
+        public static string PostgreRead()
+        {
+            if (!File.Exists(postgrefile))
+            {
+                return "";
+            }
+            StreamReader sr = new StreamReader(postgrefile);
+            string str = sr.ReadToEnd();
+            sr.Dispose();
+            sr.Close();
+
+            return str.Trim();
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            MsSqlInitializeFile();
+            mssqlConString = MsSqlRead();
+            MySqlInitializeFile();
+            mysqlConString = MySqlRead();
+            PostgreInitializeFile();
+            postgreConString = PostgreRead();
+
             Button btn = new Button();
             btn.Content = "Add";
               
--- DynamicApplication/DynamicApplication/UserControls.xaml.cs
+++ DynamicApplication/DynamicApplication/UserControls.xaml.cs
@@ -12,7 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data.SqlClient;
+using Npgsql;
+using MySql.Data.MySqlClient;
+using System.IO;
 namespace DynamicApplication
 {
     /// <summary>
@@ -26,6 +29,99 @@ namespace DynamicApplication
             AddButton();
            
         }
+        //mssql
+        private static string mssqlfile = AppDomain.CurrentDomain.BaseDirectory + "\\mssqlFile";
+        public static string mssqlConString;
+        public static SqlConnection mscon = new SqlConnection(mssqlConString);
+        //mysql
+        private static string mysqlfile = AppDomain.CurrentDomain.BaseDirectory + "\\mysqlFile";
+        public static string mysqlConString;
+        public static MySqlConnection mycon = new MySqlConnection(mysqlConString);
+
+        //postgre
+        private static string postgrefile = AppDomain.CurrentDomain.BaseDirectory + "\\postgreFile";
+        public static string postgreConString;
+        public static NpgsqlConnection pgcon = new NpgsqlConnection(postgreConString);
+
+
+        //mssql
+        public static void MsSqlInitializeFile()
+        {
+            if (!File.Exists(mssqlfile))
+            {
+                StreamWriter sw = new StreamWriter(mssqlfile);
+                sw.WriteLine("");
+                sw.Dispose();
+                sw.Close();
+            }
+        }
+
+        public static string MsSqlRead()
+        {
+            if (!File.Exists(mssqlfile))
+            {
+                return "";
+            }
+            StreamReader sr = new StreamReader(mssqlfile);
+            string str = sr.ReadToEnd();
+            sr.Dispose();
+            sr.Close();
+
+            return str.Trim();
+        }
+
+        //mysql
+        public static void MySqlInitializeFile()
+        {
+            if (!File.Exists(mysqlfile))
+            {
+                StreamWriter sw = new StreamWriter(mysqlfile);
+                sw.WriteLine("");
+                sw.Dispose();
+                sw.Close();
+            }
+        }
+
+        public static string MySqlRead()
+        {
+            if (!File.Exists(mysqlfile))
+            {
+                return "";
+            }
+            StreamReader sr = new StreamReader(mysqlfile);
+            string str = sr.ReadToEnd();
+            sr.Dispose();
+            sr.Close();
+
+            return str.Trim();
+        }
+
+        //postgre
+        public static void PostgreInitializeFile()
+        {
+            if (!File.Exists(postgrefile))
+            {
+                StreamWriter sw = new StreamWriter(postgrefile);
+                sw.WriteLine("");
+                sw.Dispose();
+                sw.Close();
+            }
+        }
+
+        public static string PostgreRead()
+        {
+            if (!File.Exists(postgrefile))
+            {
+                return "";
+            }
+            StreamReader sr = new StreamReader(postgrefile);
+            string str = sr.ReadToEnd();
+            sr.Dispose();
+            sr.Close();
+
+            return str.Trim();
+        }
+
         public void AddButton()
         {
             TextBox txtSample = new TextBox();
@@ -37,6 +133,11 @@ namespace DynamicApplication
             stackPanel.Margin = new Thickness(10);
             stackPanel.Children.Add(txtSample);
             stackPanel.Children.Add(txtSample1);
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
